cf095abde08bd9acb113032a23d3a0beba465aef
racso27th <oscar27thgbenou@gmail.com>
2026-03-05 20:54:05 +0100

    feat:add permission

--- backend/docker/start.sh
+++ backend/docker/start.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
+touch /var/www/html/database/database.sqlite
+chmod 664 /var/www/html/database/database.sqlite
 php artisan migrate --force
 php artisan config:cache
-php artisan route:cache
 /usr/bin/supervisord -c /etc/supervisord.conf
