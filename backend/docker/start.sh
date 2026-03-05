ba9f0757c9c0b6dd89124d3d39468bbb62fc1eb1
racso27th <oscar27thgbenou@gmail.com>
2026-03-05 23:49:12 +0100

    feat: add seeder to start.sh and fix permissions for sqlite database

--- backend/docker/start.sh
+++ backend/docker/start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 touch /var/www/html/database/database.sqlite
 chmod 664 /var/www/html/database/database.sqlite
-php artisan migrate --force
+php artisan migrate --force --seed --no-interaction
 php artisan config:cache
 /usr/bin/supervisord -c /etc/supervisord.conf
