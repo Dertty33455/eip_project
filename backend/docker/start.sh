2f92497fcce524842fff40ee6e917d96b117a60e
racso27th <oscar27thgbenou@gmail.com>
2026-03-06 11:57:52 +0100

    Fix permissions at runtime

--- backend/docker/start.sh
+++ backend/docker/start.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
+
+# Permissions au démarrage (pas au build)
+chmod -R 777 /var/www/html/storage
+chmod -R 777 /var/www/html/bootstrap/cache
+
+# Créer le fichier SQLite
 touch /var/www/html/database/database.sqlite
-chmod 664 /var/www/html/database/database.sqlite
-php artisan migrate --force --seed --no-interaction
-php artisan config:cache
+chmod 666 /var/www/html/database/database.sqlite
+
+# Laravel
+php artisan config:clear
+php artisan migrate --seed --force
+
+# Démarrer les services
 /usr/bin/supervisord -c /etc/supervisord.conf
